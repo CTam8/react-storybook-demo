25a5e5171b1d886faef64499df6f19f5cc4a6be3
Christian Tam <christian.tam@tjene.com>
2019-05-03 10:21:29 -0400

    prep for demo

--- components/stories/header.js
+++ components/stories/header.js
@@ -1,12 +1,3 @@
 import React from 'react';
 import Header from '../Header';
 import { storiesOf, action } from '@kadira/storybook';
-
-storiesOf('Header', module)
-  .add('default view', () => {
-    return (
-      <div className="todoapp">
-        <Header addTodo={action('Add Todo')}/>
-      </div>
-    );
-  });
